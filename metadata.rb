83668d8881c99dee70e64c612f5653175a4435dd
muratayusuke <info@muratayusuke.com>
2014-06-30 23:26:17 -0700

    bump up version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@muratayusuke.com'
 license          'All rights reserved'
 description      'Set up development environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 depends 'docker'
 depends 'git'
 depends 'global'
