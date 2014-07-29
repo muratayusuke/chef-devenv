bb3b475aa5b3fe7ee125d8747e042f27b5a12c97
muratayusuke <info@muratayusuke.com>
2014-07-29 00:12:08 -0700

    update git

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ default['dev_user']['id'] = 'devuser'
 default['dev_user']['shell'] = '/bin/zsh'
 default['dev_user']['password'] = '$1$SSk7c5ij$JggO4l1XCuWAKFLMveQoK.' # devuser
 
-default['git']['version'] = '1.9.0'
+default['git']['version'] = '2.0.3'
 default['the_silver_searcher']['version'] = '8cd9dd942d8ebe5c0b0b4593a9d64ae42fb07684'
 default['the_silver_searcher']['url'] = "https://github.com/byplayer/the_silver_searcher/archive/#{node['the_silver_searcher']['version']}.tar.gz"
 default['tmux']['install_method'] = 'source'
