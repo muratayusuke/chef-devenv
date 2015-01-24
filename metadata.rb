71810a5aec77536cd29665c917d13a535ce22353
muratayusuke <info@muratayusuke.com>
2015-01-24 13:32:59 +0900

    install git from apt instead of source

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,6 @@ description      'Set up development environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.2'
 depends 'docker'
-depends 'git'
 depends 'global'
 depends 'tmux'
 depends 'ncftp'
