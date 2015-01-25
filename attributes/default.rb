e8185ea74890d53f0efe256b725e3d3a38fa3597
muratayusuke <info@muratayusuke.com>
2015-01-25 11:00:30 +0900

    install default silver searcher

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,9 +3,6 @@ default['dev_user']['id'] = 'devuser'
 default['dev_user']['shell'] = '/bin/zsh'
 default['dev_user']['password'] = '$1$SSk7c5ij$JggO4l1XCuWAKFLMveQoK.' # devuser
 
-default['the_silver_searcher']['version'] = '8cd9dd942d8ebe5c0b0b4593a9d64ae42fb07684'
-default['the_silver_searcher']['url'] = "https://github.com/byplayer/the_silver_searcher/archive/#{node['the_silver_searcher']['version']}.tar.gz"
-
 # rbenv
 default['rbenv']['user_installs'] = [
   { 'user' => node['dev_user']['id'],
